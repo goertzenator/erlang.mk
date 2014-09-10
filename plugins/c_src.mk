03d1f3524c55f1cd3bb75d96dbff7f5338237e58
Daniel Goertzen <daniel.goertzen@gmail.com>
2014-09-10 11:28:38 -0500

    separate env functionality from compiling, add option to disable all compiling, add c++ support, compile to object files before compiling shared library.

--- plugins/c_src.mk
+++ plugins/c_src.mk
@@ -7,8 +7,7 @@
 # Configuration.
 
 C_SRC_DIR = $(CURDIR)/c_src
-C_SRC_ENV ?= $(C_SRC_DIR)/env.mk
-C_SRC_OPTS ?=
+C_SRC_ENV ?= env.mk
 C_SRC_OUTPUT ?= $(CURDIR)/priv/$(PROJECT).so
 
 # System type and C compiler/flags.
@@ -17,37 +16,100 @@ UNAME_SYS := $(shell uname -s)
 ifeq ($(UNAME_SYS), Darwin)
 	CC ?= cc
 	CFLAGS ?= -O3 -std=c99 -arch x86_64 -flat_namespace -undefined suppress -finline-functions -Wall -Wmissing-prototypes
+	CXX ?= c++
+	CXXFLAGS ?= -O3 -arch x86_64 -flat_namespace -undefined suppress -finline-functions -Wall
 else ifeq ($(UNAME_SYS), FreeBSD)
 	CC ?= cc
 	CFLAGS ?= -O3 -std=c99 -finline-functions -Wall -Wmissing-prototypes
+	CXX ?= c++
+	CXXFLAGS ?= -O3 -finline-functions -Wall
 else ifeq ($(UNAME_SYS), Linux)
 	CC ?= gcc
 	CFLAGS ?= -O3 -std=c99 -finline-functions -Wall -Wmissing-prototypes
+	CXX ?= g++
+	CXXFLAGS ?= -O3 -finline-functions -Wall
 endif
 
-# Verbosity.
 
-c_src_verbose_0 = @echo " C_SRC " $(?F);
-c_src_verbose = $(appsrc_verbose_$(V))
-
-# Targets.
-
-ifeq ($(wildcard $(C_SRC_DIR)/Makefile),)
+# generate and include env.mk
 
 app:: $(C_SRC_ENV)
-	@mkdir -p priv/
-	$(c_src_verbose) $(CC) $(CFLAGS) $(C_SRC_DIR)/*.c -fPIC -shared -o $(C_SRC_OUTPUT) \
-		-I $(ERTS_INCLUDE_DIR) $(C_SRC_OPTS)
 
 $(C_SRC_ENV):
-	erl -noshell -noinput -eval "file:write_file(\"$(C_SRC_ENV)\", \
+	$(gen_verbose) erl -noshell -noinput -eval "file:write_file(\"$(C_SRC_ENV)\", \
 		io_lib:format(\"ERTS_INCLUDE_DIR ?= ~s/erts-~s/include/\", \
 			[code:root_dir(), erlang:system_info(version)])), \
 		init:stop()."
 
+clean:: clean-c_env
+
+clean-c_env:
+	$(gen_verbose) rm -f $(C_SRC_ENV)
+
 -include $(C_SRC_ENV)
 
+CFLAGS += -I$(ERTS_INCLUDE_DIR) -fPIC
+CXXFLAGS += -I$(ERTS_INCLUDE_DIR) -fPIC
+
+
+
+# Targets.
+
+ifndef NO_AUTO_C_SRC    # if defined, disables all automatic compiling and cleaning rules
+
+ifeq ($(wildcard $(C_SRC_DIR)/Makefile),)
+
+# Compile shared object from all c and c++ sources in C_SRC_DIR
+
+
+# Object files.
+
+C_SRCS = $(wildcard $(C_SRC_DIR)/*.c)
+C_OBJS = $(C_SRCS:.c=.o)
+CPP_SRCS = $(wildcard $(C_SRC_DIR)/*.cpp $(C_SRC_DIR)/*.cc $(C_SRC_DIR)/*.C)
+CPP_OBJS = $(addsuffix .o, $(basename $(CPP_SRCS)))
+
+
+# Linker.  Use c++ linker if there are any c++ sources.
+
+ifeq ($(CPP_OBJS),)
+	LDCC ?= $(CC)
 else
+	LDCC ?= $(CXX)
+endif
+
+
+# Verbosity.
+
+cc_verbose_0  = @echo " CC    " $(notdir $<);
+cxx_verbose_0 = @echo " CXX   " $(notdir $<);
+ld_verbose_0  = @echo " LD    " $(notdir $@);
+cc_verbose  = $(cc_verbose_$(V))
+cxx_verbose = $(cxx_verbose_$(V))
+ld_verbose  = $(ld_verbose_$(V))
+
+app:: $(C_SRC_OUTPUT)
+
+$(C_SRC_OUTPUT): $(C_OBJS) $(CPP_OBJS)
+	@mkdir -p $(dir $@)
+	$(ld_verbose) $(LDCC) $(LDFLAGS) -shared -o $@ $^
+
+%.o: %.c
+	$(cc_verbose) $(CC) $(CPPFLAGS) $(CFLAGS) -c -o $@ $<
+%.o: %.cpp
+	$(cxx_verbose) $(CXX) $(CPPFLAGS) $(CXXFLAGS) -c -o $@ $<
+%.o: %.cc
+	$(cxx_verbose) $(CXX) $(CPPFLAGS) $(CXXFLAGS) -c -o $@ $<
+%.o: %.C
+	$(cxx_verbose) $(CXX) $(CPPFLAGS) $(CXXFLAGS) -c -o $@ $<
+
+clean:: clean-c_src
+
+clean-c_src:
+	$(gen_verbose) rm -f $(C_SRC_OUTPUT) $(C_OBJS) $(CPP_OBJS)
+
+
+else  # use custom makefile in c_src
 ifneq ($(wildcard $(C_SRC_DIR)),)
 
 app::
@@ -59,7 +121,5 @@ clean::
 endif
 endif
 
-clean:: clean-c_src
 
-clean-c_src:
-	$(gen_verbose) rm -f $(C_SRC_ENV) $(C_SRC_OUTPUT)
+endif #NO_AUTO_C_SRC
